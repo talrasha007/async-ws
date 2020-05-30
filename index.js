8ae90bcaa7f56d4633a01c423da761e9f57f1b5a
Tal <talrasha007@gmail.com>
2020-05-30 21:28:07 +0800

    auto reconnect option

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ module.exports = class extends EventEmitter {
   constructor(url, opt  = {}) {
     super();
 
+    const dispose = () => {
+      this._ws[removeListenerFn]('open', onopen);
+      this._ws[removeListenerFn]('close', onclose);
+      this._ws[removeListenerFn]('error', onerror);
+      this._ws[removeListenerFn]('message', onmessage);
+      delete this._ws;
+    };
+
     const onopen = () => {
       this._ready = true;
       this.emit('open');
@@ -15,27 +23,23 @@ module.exports = class extends EventEmitter {
 
     const onclose = () => {
       this._ready = false;
-      reconnect();
+      dispose();
+      this.emit('close');
+      if (this.autoReconnect) connect();
     };
 
     const onerror = (error) => {
       this._ready = false;
-      reconnect();
+      dispose();
       this.emit('error', error);
+      if (this.autoReconnect) connect();
     };
 
     const onmessage = (msg) => {
       this.emit('message', msg);
     };
 
-    const reconnect = () => {
-      if (this._ws) {
-        this._ws[removeListenerFn]('open', onopen);
-        this._ws[removeListenerFn]('close', onclose);
-        this._ws[removeListenerFn]('error', onerror);
-        this._ws[removeListenerFn]('message', onmessage);
-      }
-
+    const connect = () => {
       this._ws = new WebSocket(url, opt.options);
       this._ws[addListenerFn]('open', onopen);
       this._ws[addListenerFn]('close', onclose);
@@ -47,7 +51,12 @@ module.exports = class extends EventEmitter {
       }
     };
 
-    reconnect();
+    this.autoReconnect = opt.autoReconnect === false;
+    this._connect = connect;
+
+    if (this.autoReconnect) {
+      connect();
+    }
   }
 
   async send(data) {
@@ -55,15 +64,18 @@ module.exports = class extends EventEmitter {
     this._ws.send(data);
   }
 
-  async reconnect() {
-    if (this._ready) {
-      this._ready = false;
-      this._ws.close();
+  async close(code = 1005, reason = '') {
+    if (!this._closing && this._ws) {
+      this._closing = true;
+      this._ws.close(code, reason);
+      await new Promise(resolve => this.once('close', resolve));
+      this._closing = false;
     }
   }
 
   async ready() {
     if (!this._ready) {
+      if (!this._ws && !this.autoReconnect) this._connect();
       await new Promise(resolve => this.once('open', resolve));
     }
   }
